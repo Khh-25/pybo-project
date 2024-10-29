0830587a0275997509e493f7ff208397ef94e10d
khh <kimhoonhee25@naver.com>
2024-10-29 15:47:55 +0900

    설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default":{
+        "ENGINE" : "django.db.backends.mysql",
+        "NAME" : "MYSQL",
+        "USER" : "admin",
+        "PASSWORD" : 0000,
+        "HOST" : "127.0.0.1",
+        "PORT" : "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -194,3 +205,6 @@ LOGGING = {
         },
     },
 }
+
+
+
